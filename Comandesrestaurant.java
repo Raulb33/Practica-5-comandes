66898b048734d1e9f38120aac1623094c464cd9d
Raul-Barriga <raulbarriga658@gmail.com>
2025-11-06 17:13:15 +0100

    first commit

---
+++ Comandesrestaurant.java
@@ -0,0 +1,10 @@
+package Practica5;
+
+import java.util.Scanner;
+
+public class Comandesrestaurant {
+
+    public static void main(String[] args) {
+         Scanner p5 = new Scanner(System.in);
+    }
+}
